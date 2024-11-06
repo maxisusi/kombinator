2ab5360feb96ab1bc5efb4c17c172d7e965dfdbc
maxisusi <maxbalej@proton.me>
2024-11-06 07:22:56 +0000

    Adding documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,32 @@
 #![allow(dead_code, unused_variables)]
+//! # Parser Kombinator
+//!
+//! Kombinator is a simple parser combinator that I made
+//! to parse http request/response headers
 
+/// Generic parser errors
 #[derive(Debug, PartialEq)]
-enum ParserError {
+pub enum ParserError {
     NoMatch,
 }
+/// Parsing Result type
+pub type KResult<I, O, E = ParserError> = Result<(I, O), E>;
 
-type PResult<I, O, E = ParserError> = Result<(I, O), E>;
-
-fn tag(pattern: &str) -> impl Fn(&[u8]) -> PResult<&str, &str> + '_ {
+/// Find the pattern from source and slice
+/// the input from the index
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// // Define the pattern
+/// let parse = tag("#");
+///
+/// assert_eq!(parse(b"#123"), Ok(("123", "#")));
+/// assert_eq!(parse(b"hello#world"), Ok(("world", "#")))
+/// ```
+pub fn tag(pattern: &str) -> impl Fn(&[u8]) -> KResult<&str, &str> + '_ {
     move |source| {
         let source_to_utf8 = std::str::from_utf8(source).expect("Coulnd't transform to utf8");
 
@@ -22,13 +41,38 @@ fn tag(pattern: &str) -> impl Fn(&[u8]) -> PResult<&str, &str> + '_ {
     }
 }
 
-fn is_digit(char: char) -> bool {
+/// Checks if char is an ascii digit
+pub fn is_digit(char: char) -> bool {
     char.is_ascii_digit()
 }
-
-fn take_while_n<F>(n: usize, cond: F) -> impl Fn(&[u8]) -> PResult<&str, &str> {
-    move |source| todo!("Implementing take_while_n")
-}
+/// Iterates over the input and slice the source
+/// if it respect the condition
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// // Define the parser
+/// let parse = take_while(is_digit);
+///
+/// assert_eq!(parse(b"max123"), Ok(("23", "1")));
+/// assert_eq!(parse(b"12max"), Ok(("2max", "1")));
+/// ```
+// fn take_while<I: IntoIterator, F: Fn(I) -> bool>(
+//     condition: F,
+// ) -> impl Fn(I) -> PResult<&str, &str> {
+//     move |source| {
+//         // let source_to_utf8 = std::str::from_utf8(source).expect("Coulnd't transform to utf8");
+//         for (idx, byte) in source.into_iter().enumerate() {
+//             if condition(byte) {
+//                 return Ok((&source_to_utf8[idx + 1..], &source_to_utf8[idx..idx + 1]));
+//             }
+//         }
+//
+//         Err(ParserError::NoMatch)
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -41,20 +85,20 @@ mod tests {
         assert_eq!(parse(b"max#balej"), Ok(("balej", "#")))
     }
 
-    fn take_while_n_test() {
-        let parse = take_while_n(2, is_digit);
-
-        // assert_eq!(parse(b"max123"), Ok(("23", "max1")))
-        // assert_eq!(parse(b"1max"), Ok(("23", "max1")))
-    }
-
+    // #[test]
+    // fn take_while_n_test() {
+    //     let parse = take_while(is_digit);
+    //
+    //     assert_eq!(parse(b"max123"), Ok(("23", "1")));
+    //     assert_eq!(parse(b"12max"), Ok(("2max", "1")));
+    // }
     #[test]
     fn test_http_response_header() {
         let input = "HTTP/1.x 200 OK";
         let (input, http) = tag("HTTP")(input.as_bytes()).expect("This should work, this is a bug");
         let (input, slash) = tag("/")(input.as_bytes()).expect("This should work, this is a bug");
-        let (input, slash) =
-            take_while_n(1, is_digit)(input.as_bytes()).expect("This should work, this is a bug");
+        // let (input, slash) =
+        //     take_while_n(1, is_digit)(input.as_bytes()).expect("This should work, this is a bug");
 
         assert_eq!(http, "HTTP");
         assert_eq!(slash, "/");
