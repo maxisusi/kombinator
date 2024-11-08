2d29e181dc82e15f214ab33065d36ae990422b17
maxisusi <maxbalej@proton.me>
2024-11-08 22:45:24 +0100

    Parsing completely http header and fixing doctests

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,9 @@ pub type KResult<I, O, E = ParserError> = Result<(I, O), E>;
 ///
 /// Basic usage:
 ///
-/// ```
+/// ```rust
+/// use kombinator::tag;
+///
 /// // Define the pattern
 /// let parse = tag("#");
 ///
@@ -52,12 +54,14 @@ pub fn is_digit(char: char) -> bool {
 ///
 /// Basic usage:
 ///
-/// ```
+/// ```rust
+/// use kombinator::{take_while, is_digit};
+///
 /// // Define the parser
 /// let parse = take_while(is_digit);
 ///
-/// assert_eq!(parse(b"max123"), Ok(("23", "1")));
-/// assert_eq!(parse(b"12max"), Ok(("2max", "1")));
+/// assert_eq!(parse(b"max123"), Ok(("", "123")));
+/// assert_eq!(parse(b"12max"), Ok(("max", "12")));
 /// ```
 ///
 pub fn take_while<'a, F, I>(condition: F) -> impl Fn(&'a [u8]) -> KResult<&'a str, &'a str>
@@ -66,51 +70,22 @@ where
     F: Fn(I) -> bool,
 {
     move |source| {
-        if let Some(index) = source.iter().position(|&e| condition(e.into())) {
-            let input = std::str::from_utf8(&source[index + 1..]).expect("This should work bruh");
-            let result =
-                std::str::from_utf8(&source[index..index + 1]).expect("This should work bruh");
+        if let Some(start_index) = source.iter().position(|&e| condition(e.into())) {
+            let end_index = source[start_index..]
+                .iter()
+                .position(|ch| !condition(Into::into(*ch)))
+                .unwrap_or(source.len());
+
+            let input = std::str::from_utf8(&source[end_index..]).expect("This should work bruh");
+            let result = std::str::from_utf8(&source[start_index..end_index])
+                .expect("This should work bruh");
             Ok((input, result))
         } else {
             Err(ParserError::NoMatch)
         }
     }
 }
-/// Consume the input and slice
-/// if it matches the predicate based on length between n and m
-///
-/// # Examples
-///
-/// Basic usage:
-///
-/// ```
-/// // Define the parser
-/// let parse = take_while_n_m(2,2, is_digit);
-///
-/// assert_eq!(parse(b"mario123"), Ok(("3", "12")));
-/// assert_eq!(parse(b"12345"), Ok(("345", "12")));
-/// ```
-///
-pub fn take_while_m_n<'a, F, I>(
-    m: usize,
-    n: usize,
-    condition: F,
-) -> impl Fn(&'a [u8]) -> KResult<&'a str, &'a str>
-where
-    I: From<u8>,
-    F: Fn(I) -> bool,
-{
-    move |source| {
-        if let Some(index) = source.iter().position(|&e| condition(e.into())) {
-            let input = std::str::from_utf8(&source[index + 1..]).expect("This should work bruh");
-            let result =
-                std::str::from_utf8(&source[index..index + 1]).expect("This should work bruh");
-            Ok((input, result))
-        } else {
-            Err(ParserError::NoMatch)
-        }
-    }
-}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -126,16 +101,8 @@ mod tests {
     fn take_while_test() {
         let parse = take_while(is_digit);
 
-        assert_eq!(parse(b"max123"), Ok(("23", "1")));
-        assert_eq!(parse(b"12max"), Ok(("2max", "1")));
-    }
-
-    #[test]
-    fn take_while_n_m_test() {
-        let parse = take_while(is_digit);
-
-        assert_eq!(parse(b"max123"), Ok(("23", "1")));
-        assert_eq!(parse(b"12max"), Ok(("2max", "1")));
+        assert_eq!(parse(b"max123"), Ok(("", "123")));
+        assert_eq!(parse(b"12max"), Ok(("max", "12")));
     }
 
     #[test]
@@ -153,8 +120,10 @@ mod tests {
         let (input, number2) =
             take_while(is_digit)(input.as_bytes()).expect("This should work, this is a bug");
         let (input, space) = tag(" ")(input.as_bytes()).expect("This should work, this is a bug");
-        // let (input, slash) =
-        //     take_while_n(1, is_digit)(input.as_bytes()).expect("This should work, this is a bug");
+        let (input, two_hundred) =
+            take_while(is_digit)(input.as_bytes()).expect("This should work, this is a bug");
+        let (input, space) = tag(" ")(input.as_bytes()).expect("This should work, this is a bug");
+        let (input, ok) = tag("OK")(input.as_bytes()).expect("This should work, this is a bug");
 
         assert_eq!(http, "HTTP");
         assert_eq!(slash, "/");
@@ -162,5 +131,8 @@ mod tests {
         assert_eq!(dot, ".");
         assert_eq!(number2, "2");
         assert_eq!(space, " ");
+        assert_eq!(two_hundred, "200");
+        assert_eq!(space, " ");
+        assert_eq!(ok, "OK");
     }
 }
