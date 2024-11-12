7ff99953497b7f982dd55f34b1436b9a004c2437
maxisusi <maxbalej@proton.me>
2024-11-12 21:01:10 +0100

    Finish refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,12 @@ pub enum ParserError {
 /// Parsing Result type
 pub type KResult<I, O, E = ParserError> = Result<(I, O), E>;
 
-
-enum CompareResult {
+pub enum CompareResult {
     Ok,
-    Error
+    Error,
 }
 
-trait Compare<T> {
+pub trait Compare<T> {
     fn compare(&self, comp: T) -> CompareResult;
 }
 
@@ -36,66 +35,22 @@ impl<'a, 'b> Compare<&'b [u8]> for &'a [u8] {
                 }
             }
         }
-
     }
 }
 
-impl <'a, 'b> Compare<&'b str>  for &'a[u8] {
+impl<'a, 'b> Compare<&'b str> for &'a [u8] {
     fn compare(&self, comp: &'b str) -> CompareResult {
         self.compare(comp.as_bytes())
     }
 }
 
-impl <'a, 'b> Compare<&'b str>  for &'a str {
+impl<'a, 'b> Compare<&'b str> for &'a str {
     fn compare(&self, comp: &'b str) -> CompareResult {
         self.as_bytes().compare(comp.as_bytes())
     }
 }
 
-macro_rules! compare_impl {
-    ($($N:expr)+) => {
-        $(
-        
-            impl <'a> Compare<[u8; $N]>  for &'a[u8] {
-                fn compare(&self, comp: [u8;$N]) -> CompareResult {
-                    let res = self.iter().zip(comp.iter()).position(|(a, b)| a != b);
-
-                    match res {
-                        Some(_) => CompareResult::Error,
-                        None => {
-                            if self.len() >= comp.len() {
-                                CompareResult::Ok
-                            } else {
-                                CompareResult::Error
-                            }
-                        }
-                    }
-                }
-            }
-            impl <'a, 'b> Compare<&[u8; $N]>  for &'a[u8; $N] {
-                fn compare(&self, comp: &[u8;$N]) -> CompareResult {
-                    let res = self.iter().zip(comp.iter()).position(|(a, b)| a != b);
-
-                    match res {
-                        Some(_) => CompareResult::Error,
-                        None => {
-                            if self.len() >= comp.len() {
-                                CompareResult::Ok
-                            } else {
-                                CompareResult::Error
-                            }
-                        }
-                    }
-                }
-            }
-        )+
-        
-    };
-}
-
-compare_impl!(0 1 2 3 4 5 6 7 8 9 10);
-
-trait InputLength {
+pub trait InputLength {
     fn input_length(&self) -> usize;
 }
 
@@ -112,67 +67,30 @@ impl<'a> InputLength for &'a [u8] {
     }
 }
 
-
-macro_rules! input_len_impl {
-    ($($N:expr)+) => {
-        $(
-            impl<'a> InputLength for &'a [u8; $N] {
-                fn input_length(&self) -> usize {
-                    self.len()
-                }
-            }
-        )+
-    }
-}
-
-input_len_impl!(1 2 3 4 5 6 7 8 9 10);
-
-trait InputTake: Sized {
+pub trait InputTake: Sized {
     fn take(&self) -> Self;
-    fn take_split(&self, count: usize) -> (Self,Self);
+    fn take_split(&self, count: usize) -> (Self, Self);
 }
 
-impl <'a> InputTake for &'a str {
-    fn take_split(&self, count: usize) -> (Self,Self) {
-        (&self[..count], &self[count..])
+impl<'a> InputTake for &'a str {
+    fn take_split(&self, count: usize) -> (Self, Self) {
+        let (prefix, suffix) = self.split_at(count);
+        (suffix, prefix)
     }
     fn take(&self) -> Self {
         todo!()
     }
 }
 
-
-
-impl <'a> InputTake for &'a [u8] {
-    fn take_split(&self, count: usize) -> (Self,Self) {
-        let (prefix, sufix) = self.split_at(count);
-        (prefix, sufix)
+impl<'a> InputTake for &'a [u8] {
+    fn take_split(&self, count: usize) -> (Self, Self) {
+        let (prefix, suffix) = self.split_at(count);
+        (suffix, prefix)
     }
     fn take(&self) -> Self {
         self
     }
 }
-
-// macro_rules! input_take_impl {
-//     ($($N:expr)+) => {
-//         $(
-//             impl <'a> InputTake for &'a [u8;$N] {
-//                 fn take_split(&self, count: usize) -> (Self,Self) {
-//                     (&self[0..count], &self[count..])
-//                 }
-//                 fn take(&self) -> Self {
-//                     self
-//                 }
-//             }
-//         )+ 
-//     };
-// }
-//
-// input_take_impl!(1 2 3 4 5 6 7 8 9 10);
-
-
-
-
 /// Match the the pattern
 /// from the start of the source until
 /// pattern lenght
@@ -225,14 +143,14 @@ pub fn matching<'a>(pattern: &'a str) -> impl Fn(&'a [u8]) -> KResult<&'a str, &
 pub fn tag<I, E>(pattern: E) -> impl Fn(I) -> KResult<I, I>
 where
     E: Clone + InputLength,
-    I: Compare<E> + InputTake
+    I: Compare<E> + InputTake,
 {
     move |source| {
         let compare_result = source.compare(pattern.clone());
         let pattern_len = pattern.input_length();
-        return match compare_result {
-            CompareResult::Ok => { Ok(source.take_split(pattern_len)) },
-            CompareResult::Error => Err(ParserError::NoMatch)
+        match compare_result {
+            CompareResult::Ok => Ok(source.take_split(pattern_len)),
+            CompareResult::Error => Err(ParserError::NoMatch),
         }
     }
 }
@@ -285,22 +203,15 @@ mod tests {
     use super::*;
 
     #[test]
-    // fn matching_test() {
-    //     let parse = matching("http");
-    //     assert_eq!(parse(b"http 1"), Ok((" 1", "http")));
-    // }
-
-    #[test]
     fn tag_test() {
-        let parse = tag("#");
-        let parsed = parse("#123");
-        // assert_eq!(parse(b"#123"), Ok((b"123", b"#")));
-        // assert_eq!(parse(b"max#balej"), Ok(("balej", "#")));
+        let parse_hashtag = tag("#");
+        assert_eq!(parse_hashtag("#123"), Ok(("123", "#")));
+        assert_eq!(parse_hashtag("#hello world"), Ok(("hello world", "#")));
 
-        let parse = tag("http")("hello");
+        let parse_http = tag("http");
 
-        // assert_eq!(parse(b"httvp"), Err(ParserError::NoMatch));
-        // assert_eq!(parse(b"http"), Ok(("", "http")));
+        assert_eq!(parse_http("httvp"), Err(ParserError::NoMatch));
+        assert_eq!(parse_http("httpisking"), Ok(("isking", "http")));
     }
 
     #[test]
@@ -314,8 +225,6 @@ mod tests {
     #[test]
     fn test_http_response_header() {
         let input = "HTTP/1.2 200 OK";
-
-        input.as_bytes()
 
         let (input, http) =
             matching("HTTP")(input.as_bytes()).expect("This should work, this is a bug");
